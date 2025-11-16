d54c51fc0cf9b0720815cd9d3e5bc23118faf7f8
Michael cheng <mikeypeter37@gmail.com>
2025-11-16 15:23:59 +0800

    poop

--- ft_printf.c
+++ ft_printf.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nigger <nigger@gmail.com>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/16 15:19:10 by nigger            #+#    #+#             */
+/*   Updated: 2025/11/16 15:19:10 by nigger           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
 int	ft_format(char c, va_list args)
@@ -7,11 +19,11 @@ int	ft_format(char c, va_list args)
 	else if (c == 's')
 		return (ft_putstr_pf(va_arg(args, char *)));
 	else if (c == 'd' || c == 'i')
-		return ft_putnbr_pf((long)va_arg(args, int));
+		return (ft_putnbr_pf((long)va_arg(args, int)));
 	else if (c == 'u')
-		return ft_putunbr_pf((unsigned long)va_arg(args, unsigned int));
+		return (ft_putunbr_pf((unsigned long)va_arg(args, unsigned int)));
 	else if (c == 'p')
-		return ft_putptr_pf(va_arg(args, void *));
+		return (ft_putptr_pf(va_arg(args, void *)));
 	else if (c == 'x')
 		return (ft_puthex_pf(va_arg(args, unsigned int), HEX_LOW));
 	else if (c == 'X')
